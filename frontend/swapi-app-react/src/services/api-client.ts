a8b1a422f237b74605e0f6863ba0ddd2c3066f0c
Daniel Larsson <eadaniel.larsson@gmail.com>
2024-06-16 08:20:14 +0200

    Updated baseURL with Railway URL

--- frontend/swapi-app-react/src/services/api-client.ts
+++ frontend/swapi-app-react/src/services/api-client.ts
@@ -2,7 +2,7 @@ import axios, { AxiosError, CanceledError } from "axios";
 
 export default axios.create({
   // baseURL: 'http://localhost:3000/api/people'
-  baseURL: 'http://localhost:3000/api/people'
+  baseURL: 'https://swapi-app-production.up.railway.app/api/people'
 });
 
 export { AxiosError, CanceledError };
