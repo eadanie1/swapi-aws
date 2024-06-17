425974713d905137f732f72f9058278e4ef0a428
Daniel Larsson <eadaniel.larsson@gmail.com>
2024-06-17 13:08:05 +0200

    Removed localhost commented out URL

--- frontend/swapi-app-react/src/services/api-client.ts
+++ frontend/swapi-app-react/src/services/api-client.ts
@@ -1,7 +1,6 @@
 import axios, { AxiosError, CanceledError } from "axios";
 
 export default axios.create({
-  // baseURL: 'http://localhost:3000/api/people'
   baseURL: 'https://swapi-app-production.up.railway.app/api/people'
 });
 
